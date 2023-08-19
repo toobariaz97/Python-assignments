10cdd85f65de3a8f680da69630cd73793e0a8919
Tooba Riaz <65142828+toobariaz97@users.noreply.github.com>
2023-08-19 21:22:13 +0500

    adding quetion 3

--- chooseNumerValue.py
+++ chooseNumerValue.py
@@ -1,21 +1,41 @@
+
+
+# Simple
+
 def has_numeric_value(lst):
     for item in lst:
         if isinstance(item, (int, float)):
             return True
     return False
 
-user_list = input("Enter a list of values (separated by spaces): ").split()
+# Example list
+my_list = [1, 'apple', 3.14, 'banana', True, 42]
 
-for i in range(len(user_list)):
-    try:
-        user_list[i] = int(user_list[i])
-    except ValueError:
-        try:
-            user_list[i] = float(user_list[i])
-        except ValueError:
-            pass
-
-if has_numeric_value(user_list):
+if has_numeric_value(my_list):
     print("The list contains at least one numeric value.")
 else:
     print("The list does not contain any numeric values.")
+    
+    
+    # Advance
+# def has_numeric_value(lst):
+#     for item in lst:
+#         if isinstance(item, (int, float)):
+#             return True
+#     return False
+
+# user_list = input("Enter a list of values (separated by spaces): ").split()
+
+# for i in range(len(user_list)):
+#     try:
+#         user_list[i] = int(user_list[i])
+#     except ValueError:
+#         try:
+#             user_list[i] = float(user_list[i])
+#         except ValueError:
+#             pass
+
+# if has_numeric_value(user_list):
+#     print("The list contains at least one numeric value.")
+# else:
+#     print("The list does not contain any numeric values.")
---
+++ valueDitionary.py
@@ -0,0 +1,35 @@
+
+# Basic
+my_dict = {
+    "name": "John",
+    "age": 30,
+    "city": "New York"
+}
+
+# Adding a new key-value pair
+my_dict["occupation"] = "Engineer"
+
+print(my_dict)
+
+
+
+
+# Advance
+# my_dict = {}
+# # Function to add key-value pairs
+# def add_to_dictionary(dictionary, key, value):
+#     dictionary[key] = value
+
+# # Allow the user to add values to the dictionary
+# while True:
+#     key = input("Enter key (or 'exit' to stop): ")
+    
+#     if key.lower() == 'exit':
+#         break
+    
+#     value = input("Enter value: ")
+    
+#     add_to_dictionary(my_dict, key, value)
+#     print("Key-value pair added.")
+
+# print("Final dictionary:", my_dict)
